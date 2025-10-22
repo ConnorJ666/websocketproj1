b6d94c7846b256e0f9fb8abac7859deebb23e0eb
connor <cjohnson2@mines.edu>
2025-10-22 15:15:58 -0600

    more socket work, now handles disconnects well

--- index.js
+++ index.js
@@ -3,10 +3,29 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database
+});
+
+await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+    );
+`);
 
 const app = express();
 const server = createServer(app);
-const io =  new Server(server);
+const io =  new Server(server, {
+    connectionStateRecovery: {
+
+    }
+});
 
 const _dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -15,14 +34,41 @@ app.get('/', (req, res) => {
 
 });
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
     console.log('a user connected');
+    if(socket.recovered) {
+        console.log("recover");
+    }
+    else{
+        console.log("normal");
+    }
     socket.on('disconnect', () => {
+        
         console.log("disconnection");
     });
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    })
+    socket.on('chat message', async (msg) => {
+        let result;
+        try{
+            result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+        } catch (e) {
+            //TODO handle error
+            return;
+        }
+        io.emit('chat message', msg, result.lastID);
+    });
+    if(!socket.recovered) {
+        try{
+            await db.each('SELECT id, content FROM messages WHERE id > ?',
+                [socket.handshake.auth.serverOffset || 0],
+                (_err, row) => {
+                    socket.emit('chat message', row.content, row.id);
+                }
+            )
+        }
+        catch(e) {
+
+        }
+    }
 });
 
 server.listen(3000, () => {
